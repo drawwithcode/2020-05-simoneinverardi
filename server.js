139f382eea55b86ce3ea75429e51ad8782d9bfc1
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-24 11:44:44 +0100

    math

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function mouseMessage(dataReceived) {
 
 function getRandomColor() {
   let myColors = ["#FF7F50", "#6495ED", "#DC143C", "#FFD700", "#4682B4"];
-  color = random(myColors.length);
+  color = Math.random(myColors.length);
 
   // let letters = "0123456789ABCDEF";
   // let color = "#";
