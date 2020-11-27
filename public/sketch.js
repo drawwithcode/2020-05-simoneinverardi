f802be5b3d2fbdd90497d7b2c1087cd87f6eb3c0
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-27 23:46:34 +0100

    sliders colors

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,4 @@
 let socket = io();
-// let myColor = "white";
 
 socket.on("connect", newConnection);
 socket.on("mouseBroadcast", drawOtherMouse);
@@ -38,13 +37,12 @@ function newConnection (){
 function drawOtherMouse(data){
   push();
   noStroke();
-  specularMaterial(250, 250, 250, 80);
+  specularMaterial(250, 250, 250, data.val);
   translate(data.x, data.y, -1000);
   rotateX(data.rot);
   rotateY(data.rot);
   rotateZ(data.rot);
-  ellipsoid(30, 8, 8);
-
+  ellipsoid(data.val1, data.val2, 12);
   pop();
 }
 
@@ -53,31 +51,53 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight, WEBGL)
+  let cnv = createCanvas(1200, 1000, WEBGL);
+  let xCnv = (windowWidth - width) / 2;
+  let yCnv = (windowHeight - height) / 2;
+  cnv.position(xCnv, yCnv);
   background(255);
 
-  slider = createSlider(50, 200, 50);
+  slider = createSlider(50, 150, 50);
   slider.position(10, 10);
   slider.style('width', '80px');
+  // slider.parent("sliders");
 
   slider1 = createSlider(45, 255, 45);
-  slider1.position(10, 30);
+  slider1.position(10, 50);
   slider1.style('width', '80px');
+  // slider1.parent("sliders");
   slider2 = createSlider(12, 255, 12);
-  slider2.position(10, 50);
+  slider2.position(10, 70);
   slider2.style('width', '80px');
-  // slider3 = createSlider(12, 255, 12);
-  // slider3.position(10, 70);
-  // slider3.style('width', '80px');
+  // slider2.parent("sliders");
 
+
+  slider3 = createSlider(10, 250, 200);
+  slider3.position(10, 110);
+  slider3.style('width', '80px');
+  // slider3.parent("sliders");
+  slider4 = createSlider(10, 255, 50);
+  slider4.position(10, 130);
+  slider4.style('width', '80px');
+  // slider4.parent("sliders");
+  slider5 = createSlider(10, 255, 0);
+  slider5.position(10, 150);
+  slider5.style('width', '80px');
+  // slider5.parent("sliders");
 }
 
 function draw() {
+
+    let color1 = slider3.value();
+    let color2 = slider4.value();
+    let color3 = slider5.value();
+
   // put drawing code here
-    pointLight(200, 50, 0, -width/2, 0, 0);
+    pointLight(color1, color2, color3, -width/2, 0, 0);
     // pointLight(0, 50, 200, -width/4, 0, 0);
-    pointLight(0, 50, 200, width/2, 0, 0);
+    pointLight(-color3, color2, -color1, width/2, 0, 0);
 }
+
 
 function mouseMoved() {
   push();
@@ -89,7 +109,7 @@ function mouseMoved() {
   // background(255,10);
   let locX = mouseX - width / 2;
   let locY = mouseY - height / 2;
-  let locRot = frameCount*0.1;
+  let locRot = frameCount * 0.08;
 
   noStroke();
   specularMaterial(250, 250, 250, val);
@@ -106,6 +126,9 @@ function mouseMoved() {
     x: locX,
     y: locY,
     rot: locRot,
+    val: val,
+    val1: val1,
+    val2: val2,
   };
   //send to the server
   socket.emit("mouse", message);
--- server.js
+++ server.js
@@ -39,5 +39,5 @@ function getRandomColor() {
   for ( let i = 0; i < 6; i++) {
   color += letters[Math.floor(Math.random() * 16)];
   }
-  return color;
+return color;
 }
